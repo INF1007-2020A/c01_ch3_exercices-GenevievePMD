6dbde9b90a8d789941838dc9a0a449d167ab15af
Genevieve Pelletier-Mc Duff <genevieve.pelletier-mc-duff@polymtl.ca>
2020-09-14 14:43:07 -0400

    Ajout fonctions to_celsius et to_farenheit

--- exercice.py
+++ exercice.py
@@ -21,11 +21,11 @@ def to_degrees(angle_rads: float) -> tuple:
 
 
 def to_celsius(temperature: float) -> float:
-    return 0.0
+    return (temperature - 32)*(5/9)
 
 
 def to_farenheit(temperature: float) -> float:
-    return 0.0
+    return (temperature * (9/5)) + 32
 
 
 def main() -> None:
